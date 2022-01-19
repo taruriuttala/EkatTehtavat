c349f54c89c96c14995bdbc6688cd8dd9d811e86
unknown <taru.riuttala@gmail.com>
2022-01-19 13:47:59 +0200

    lisatty tehtava2 samaan

--- src/main/java/kevat22/tehtava1/web/Tehtava1Controller.java
+++ src/main/java/kevat22/tehtava1/web/Tehtava1Controller.java
@@ -2,6 +2,7 @@ package kevat22.tehtava1.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -16,6 +17,13 @@ public class Tehtava1Controller {
 	@ResponseBody
 	public String contactPage() {
 		return("This is the contact page");
+	}
+	
+	@RequestMapping("hello")
+	@ResponseBody
+	public String sayWelcome(@RequestParam(name="location", required=false, defaultValue = "Porvoo") String paikka,
+			@RequestParam(name="name", required=false, defaultValue = "Runeberg") String nimi) {
+		return "Welcome to " + paikka + " " + nimi + "!";
 	}
 
 }
